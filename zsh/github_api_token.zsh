7286b4fee941254ac9b43cf936e2ddf00aac0717
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-09-02 11:54:14 +0100

    Add other github api token

--- zsh/github_api_token.zsh
+++ zsh/github_api_token.zsh
@@ -1,2 +1,4 @@
 filename="$HOME/.secrets/homebrew_github_token"
 [[ -e $filename ]] && export HOMEBREW_GITHUB_API_TOKEN=$(cat $filename)
+filename="$HOME/.secrets/github_token"
+[[ -e $filename ]] && export GITHUB_API_TOKEN=$(cat $filename)
