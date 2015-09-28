4ecccc411f2f12b236753022adf9eb49427c81a1
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-09-28 14:09:52 +0100

    Adding some enviromnent flags £

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,4 +1,7 @@
 export DEFAULT_USER=$(whoami)
-export CFLAGS=-I/opt/boxen/homebrew/include/
-export CXXFLAGS=-I/opt/boxen/homebrew/include/
+export CFLAGS="-I/opt/boxen/homebrew/include/ -Wall"
+export CXXFLAGS="$CFLAGS"
 export LDFLAGS=-L/opt/boxen/homebrew/lib/
+export EDITOR=vim
+export VISUAL=gvim
+export CLICOLOR=1
