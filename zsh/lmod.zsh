63248b2331024fa7ec6873f3dbfcb0f2b4e119b0
Mayeul d'Avezac <mdavezac@gmail.com>
2016-01-09 17:14:26 +0000

    prefix diff between boxen and salt

--- zsh/lmod.zsh
+++ zsh/lmod.zsh
@@ -1,2 +1,5 @@
-. /opt/boxen/homebrew/Cellar/lmod/5.9.3/init/zsh
+[[ -e /opt/boxen/homebrew/Cellar/lmod/5.9.3/init/zsh ]] && \
+  source /opt/boxen/homebrew/Cellar/lmod/5.9.3/init/zsh
+[[ -e /usr/local/Cellar/lmod/5.9.3/init/zsh ]] && \
+  source /usr/local/Cellar/lmod/5.9.3/init/zsh
 module use $HOME/.lmodfiles
