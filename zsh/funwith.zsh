a880b2c50df6a1e88d94071cbd3902f9769dc2fe
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2016-01-13 09:50:48 +0000

    Other place whith old funwith module directory

--- zsh/funwith.zsh
+++ zsh/funwith.zsh
@@ -2,7 +2,7 @@
   source /opt/boxen/homebrew/Cellar/lmod/5.9.3/init/zsh
 [[ -e /usr/local/Cellar/lmod/5.9.3/init/zsh ]] && \
   source /usr/local/Cellar/lmod/5.9.3/init/zsh
-if [[ -e "~/.lmodfiles" ]] ; then
+if [[ -e ~/.lmodfiles ]] ; then
   module use $HOME/.lmodfiles
 else
   module use $HOME/.funwith
