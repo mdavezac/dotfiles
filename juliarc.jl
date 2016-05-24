f45d5fa53a683ee3ff3c90b72de6986887d5a168
Mayeul d'Avezac <mdavezac@gmail.com>
2016-05-24 16:37:59 +0100

    Adding history options to zsh

--- juliarc.jl
+++ juliarc.jl
@@ -1,6 +1,6 @@
-using FactCheck
-function retest(x::String)
-  FactCheck.clear_results()
-  reload(x)
-  include(joinpath(Pkg.dir(x), "test", "runtests.jl"))
-end
+# using FactCheck
+# function retest(x::String)
+#   FactCheck.clear_results()
+#   reload(x)
+#   include(joinpath(Pkg.dir(x), "test", "runtests.jl"))
+# end
--- zsh/env.zsh
+++ zsh/env.zsh
@@ -6,4 +6,11 @@ export VISUAL=gvim
 export CLICOLOR=1
 export HISTFILE=~/.zhistory
 export HISTSIZE=100000
-export HISTSAVE=100000
+export SAVEHIST=100000
+
+setopt SHARE_HISTORY
+setopt HIST_FIND_NO_DUPS
+setopt HIST_IGNORE_SPACE
+setopt HIST_NO_STORE
+setopt HIST_SAVE_NO_DUPS
+setopt HIST_REDUCE_BLANKS
