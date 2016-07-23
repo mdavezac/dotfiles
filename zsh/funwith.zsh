2ed28623e8e5198e3c77f37180a9d7b4e24dae09
Mayeul d'Avezac <mdavezac@gmail.com>
2016-07-23 18:23:48 +0100

    Simplifies lmod stuff

--- zsh/funwith.zsh
+++ zsh/funwith.zsh
@@ -1,7 +1,3 @@
 [[ -e /usr/local/Cellar/lmod/5.9.3/init/zsh ]] && \
   source /usr/local/Cellar/lmod/5.9.3/init/zsh
-if [[ -e ~/.lmodfiles ]] ; then
-  module use $HOME/.lmodfiles
-else
-  module use $HOME/.funwith
-fi
+module use $HOME/.funwith
